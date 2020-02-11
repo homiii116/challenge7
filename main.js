05edb3ed7cc768b9ba75c50b1e0a4d0815b06e2a
miho yamagata <miho.yamagata@gmail.com>
2020-02-11 17:09:59 +0900

    submit

--- main.js
+++ main.js
@@ -2,21 +2,30 @@
 // alertで'You got 5 guesses. Guess 1 to 5'と言うメッセージを表示させ、１から５までの数値を当てるよう指示する。
 alert('You got 5 guesses. Guess 1 to 5');
 
+const numbers = [1, 3, 4, 5];
+
 // 答えの数値を2で用意
-const answer = 2;
+let answer = [2];
 
 // promptで'What do you guess?'と言うメッセージとともに、記入欄を表示させる
-const userInput = prompt('What do you guess?');
+let userInput = prompt('What do you guess?');
 
 // forループで１から５までの数値で、答えの2が当たるまで推測を促すpromptを表示させる。
 // ヒント１：if文とも組み合わせて、推測が当たった場合はalertで'Good job! See what happens next :)'を表示。当たったらゲーム終了にする。
 // ヒント２：外れたらpromptで'Ops! Maybe, another try?'のメッセージとともに記入欄を表示。
-for (let i = 0; i <= 5; i++) {
+for (let i = 0; numbers.length; i++) {
     if (userInput == answer) {
-        alert('Good job! See what happens next :');
-        break;
-        } else {
-        prompt('Ops! Maybe, another try?');
+      alert('Good job! See what happens next :)');
+      const image = document.getElementById('image'); 
+      image.innerHTML = `
+        <h1>You made it! 🎮</h1>
+        <iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p>
+      `;
+      break;
+    } else if (userInput == numbers[0] || userInput == numbers[1] || userInput == numbers[2] || userInput == numbers[3]) {
+        userInput = prompt('Ops! Maybe, another try?');
+    } else {
+        userInput = prompt('Use 1 to 5 guess an answer!');
     }
 }
 
@@ -24,16 +33,24 @@ for (let i = 0; i <= 5; i++) {
 // for文で完成したら、for文箇所をコメントアウトし、while文で書き換えてみましょう。
 // let i = 0;
 
-// while (i <= 5) {
+// while (i < 4) {
 //     if (userInput == answer) {
-//         alert('Good job! See what happens next :');
-//         break;
+//       alert('Good job! See what happens next :');
+//       const image = document.getElementById('image'); 
+//       image.innerHTML = `
+//         <h1>You made it! 🎮</h1>
+//         <iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p>
+//       `;
+//       break;
+//     } else if (userInput == numbers[0] || userInput == numbers[1] || userInput == numbers[2] || userInput == numbers[3]) {
+//         userInput = prompt('Ops! Maybe, another try?');
 //     } else {
-//         prompt('Ops! Maybe, another try?');
+//         userInput = prompt('Use 1 to 5 guess an answer!');
 //     }
 // }
 
 const image = document.getElementById('image'); 
 image.innerHTML = `
+  <h1>You made it! 🎮</h1>
   <iframe src="https://giphy.com/embed/11sBLVxNs7v6WA" width="480" height="217" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cheer-cheering-11sBLVxNs7v6WA"></a></p>
 `;
